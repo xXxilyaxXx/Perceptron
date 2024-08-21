4350aaf7d5375bcf39262e27fb8b4cf8d11a085e
ilya shimanskiy <96175686+xXxilyaxXx@users.noreply.github.com>
2024-08-21 14:21:19 +0300

    Add files via upload

---
+++ Perceptron.py
@@ -0,0 +1,33 @@
+import numpy as np 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+trainig_inputs = np.array([[0,0,1],
+                           [1,1,1],
+                           [1,0,1],
+                           [0,1,1]])
+
+trainig_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3,1)) - 1
+
+print('Случайные инициализирующие веса:')
+print(synaptic_weights)
+
+#Метод обратного распространения 
+for i in range(20000):
+    input_layer = trainig_inputs
+    outputs = sigmoid( np.dot(input_layer, synaptic_weights))
+
+    err = trainig_outputs - outputs
+    adjustments = np.dot( input_layer.T, err * (outputs * (1 - outputs)) )
+
+    synaptic_weights += adjustments
+
+print("Веса после обучения:")
+print(synaptic_weights)
+print("Результат после обучения после обучения:")
+print(outputs)
